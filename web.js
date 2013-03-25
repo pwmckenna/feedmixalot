ce47caac42091b81cefb2daa3f31d4b5978375b7
Patrick Williams <pwmckenna@gmail.com>
2013-03-25 10:16:45 -0700

    using todium new app id/secret for analytics

--- web.js
+++ web.js
@@ -6,16 +6,15 @@ var Firebase = require('./lib/firebase-node')
 var FirebaseTokenGenerator = require("./lib/firebase-token-generator-node.js");
 var q = require('q');
 var request = require('request');
+var assert = require('assert');
+assert(process.env.TODIUM_API_ID, 'must provide a todium api id to track torrent statistics');
+assert(process.env.TODIUM_API_SECRET, 'must provide a todium api secret to track torrent statistics');
 
 var app = express();
-var tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET);
-var token = tokenGenerator.createToken({}, {
-    admin: true
-});
 
 // Turns the standard node function signature with a callback argument
 // into a function that returns a deferred object
-var promisify = function(nodeAsyncFn, context) {
+var promisify = function (nodeAsyncFn, context) {
   return function() {
     var defer = new q.defer()
       , args = Array.prototype.slice.call(arguments);
@@ -35,39 +34,12 @@ var promisify = function(nodeAsyncFn, context) {
 };
 // Wrap our get requests so it plays nicely with all our other
 // defered object code
-var get = promisify(request.get);
+var post = promisify(request.post);
 
-var cachedGet = function(cache, key, request) {
-    console.log('cachedGet', cache, key, request);
-    var defer = new q.defer();
-    var promise = defer.promise;
-    var cachedUrl = cache.child(key);
-    cachedUrl.once('value', function(valueSnapshot) {
-        var cachedUrlValue = valueSnapshot.val();
-        console.log('cachedGet value', cachedUrlValue);
-        if(cachedUrlValue) {
-            defer.resolve(cachedUrlValue);
-        } else {
-            get(request).then(function(response) {
-                console.log(response);
-                console.log('storing cached response', response.body);
-                cachedUrl.set(response.body);
-                defer.resolve(response.body);
-            }, function(err) {
-                console.log('storing cached response failed', err);
-                defer.reject(err);
-            });
-        }
+var trackTorrentLinks = function (node, feedName) {
+    var deferreds = _.map(node.childNodes(), function(child) {
+        return trackTorrentLinks(child, feedName);
     });
-
-    return promise;
-}
-
-var trackTorrentLinks = function(node, token, feedName, cache) {
-    console.log('trackTorrentLinks');
-    var d = new q.defer();
-    d.resolve();
-    var ret = d.promise;
 
     if(node.name() === 'enclosure' && 
         node.attr('type') && node.attr('type').value() === 'application/x-bittorrent' && 
@@ -75,47 +47,32 @@ var trackTorrentLinks = function(node, token, feedName, cache) {
     ) {
         var url = node.attr('url').value();
         console.log('converting', url);
-
-        var labels = [
-            'feedmixalot', 
-            feedName,
-            url
-        ];
-
-        ret = ret.then(function() {
-            var apiUrl = 'http://api.todium.com';
-            var key = new Buffer(url).toString('base64')
-            var trackRequest = cachedGet(cache, key, {
-                url: apiUrl,
-                qs: {
-                    src: url,
-                    token: token,
-                    labels: labels
-                }
-            });
-            return trackRequest.then(function(body) {
-                node.attr('url', body);
-            });
+        var apiUrl = 'http://api.todium.com';
+        var trackRequest = post({
+            url: apiUrl,
+            form: {
+                src: url,
+                id: process.env.TODIUM_API_ID,
+                secret: process.env.TODIUM_API_SECRET
+            }
         });
+        var rssNodeEdit = trackRequest.then(function(res) {
+            if(res.statusCode === 200) {
+                console.log('successfully replaced torrent url with a todium link', url, res.body);
+                node.attr('url', res.body);
+                return q.resolve();
+            } else {
+                console.error('failed to replace torrent url with a todium link', url);
+                return q.resolve();
+            }
+        });
+        deferreds.push(rssNodeEdit);
     } 
 
-    _.each(node.childNodes(), function(child) {
-        ret = ret.then(_.partial(trackTorrentLinks, child, token, feedName, cache));
-    });
-
-    return ret;
+    return q.all(deferreds);
 }
 
-var onFirebaseLogin = function(error, dummy) {
-    if(error) {
-        console.log('firebase login failure', error);
-    } else {
-        console.log('firebase login success');
-    }
-};
-
-var firebase = new Firebase('https://featuredcontent.firebaseIO.com/');
-firebase.auth(token, onFirebaseLogin);
+var firebase = new Firebase('https://rss.firebaseio.com/');
 
 var headers = {
     'content-type': 'application/rss+xml; charset=utf-8',
@@ -125,9 +82,8 @@ var headers = {
 app.get('/:link', function(req, res) {
     console.log(req.params.link);
     var link = firebase.child('rss').child(req.params.link);
-
     link.once('value', function(linkSnapshot) {
-        console.log('link value', link, linkSnapshot.val());
+        console.log('link value', link.name(), linkSnapshot.val());
         if(!_.isObject(linkSnapshot.val())) {
             res.writeHead(500, headers);
             res.end();
@@ -140,13 +96,11 @@ app.get('/:link', function(req, res) {
         console.log('user', user);
         console.log('feed', feed);
 
-        firebase.child('users').child(user.provider).child(user.id).once('value', function(userSnapshot) {
+        firebase.child('users').child(user.provider).child(user.id).once('value', function (userSnapshot) {
             console.log('user value', user)
 
             var feedInfo = userSnapshot.val()['feeds'][feed];
-            var userToken = userSnapshot.val()['token'];
             console.log('feedInfo', feedInfo);
-            console.log('userToken', userToken);
 
             var name = feedInfo.name;
             var urls = _.pluck(_.values(feedInfo.urls), 'url');
@@ -165,24 +119,29 @@ app.get('/:link', function(req, res) {
                 title: name
             });
 
-            var cache = link.child('cache');
-
-            aggregateRequest.then(function(aggregate) {
-                //trackTorrentLinks(aggregate, userToken, name, cache).then(function() {
+            aggregateRequest.then(function (aggregate) {
+                console.log('aggregate', name, aggregate.toString());
+                return trackTorrentLinks(aggregate, name).then(function () {
+                    console.log('replaced all torrent links');
                     res.writeHead(200, headers);
                     var body = aggregate.toString();
-                    res.end(body);
-                //});
-            }, function(err) {
+                    res.end(body);  
+                    return q.resolve();
+                }, function (err) {
+                    res.writeHead(err, headers);
+                    res.end('failed to replace torrent links');
+                    return q.reject(err);
+                });
+            }, function (err) {
                 res.writeHead(err, headers);
                 res.end('failed to aggreate requests');
-                return;
+                return q.reject(err);
             });
         });
     });
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('listening on port ' + port);    
 });
